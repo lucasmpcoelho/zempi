72f1f5e73bf4dfc7a2db55eb80db388ec9acf7bc
Lucas M P Coelho <lucasmpcoelho@Lucass-MacBook-Air.local>
2025-12-18 17:42:13 -0300

    Remove debug instrumentation from build script

--- script/build.ts
+++ script/build.ts
@@ -1,18 +1,6 @@
-// #region agent log
-console.log("[BUILD] Script starting - imports beginning...");
-// #endregion
 import { build as esbuild } from "esbuild";
-// #region agent log
-console.log("[BUILD] esbuild imported");
-// #endregion
 import { build as viteBuild } from "vite";
-// #region agent log
-console.log("[BUILD] vite imported");
-// #endregion
 import { rm, readFile } from "fs/promises";
-// #region agent log
-console.log("[BUILD] fs/promises imported - all imports complete");
-// #endregion
 
 // server deps to bundle to reduce openat(2) syscalls
 // which helps cold start times
@@ -45,26 +33,10 @@ const allowlist = [
 ];
 
 async function buildAll() {
-  // #region agent log
-  console.log("[BUILD] buildAll() started");
-  // #endregion
-  
-  // #region agent log
-  console.log("[BUILD] Removing dist directory...");
-  // #endregion
   await rm("dist", { recursive: true, force: true });
-  // #region agent log
-  console.log("[BUILD] dist directory removed");
-  // #endregion
 
   console.log("building client...");
-  // #region agent log
-  console.log("[BUILD] Starting viteBuild()...");
-  // #endregion
   await viteBuild();
-  // #region agent log
-  console.log("[BUILD] viteBuild() completed");
-  // #endregion
 
   console.log("building server...");
   const pkg = JSON.parse(await readFile("package.json", "utf-8"));
@@ -89,20 +61,7 @@ async function buildAll() {
   });
 }
 
-// #region agent log
-console.log("[BUILD] Calling buildAll()...");
-// #endregion
-buildAll()
-  .then(() => {
-    // #region agent log
-    console.log("[BUILD] buildAll() promise resolved successfully");
-    // #endregion
-  })
-  .catch((err) => {
-    // #region agent log
-    console.error("[BUILD] ERROR in buildAll():", err);
-    console.error("[BUILD] Error stack:", err?.stack);
-    // #endregion
-    console.error(err);
-    process.exit(1);
-  });
+buildAll().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
